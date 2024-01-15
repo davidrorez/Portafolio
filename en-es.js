637f2e28120d2321b2d774befda0838da4addfea
davidrorez <dvd.42liga@gmail.com>
2024-01-14 20:42:28 -0600

    translate WIP

--- en-es.js
+++ en-es.js
@@ -1,19 +1,39 @@
-var languageFlag = document.getElementById('current-lang');
-var enButton = document.getElementById('en-btn');
-var esButton = document.getElementById('es-btn');
-var i18n = domI18n({
-  selector: '[data-translatable]',
-  separator: ' // ',
-  languages: ['en', 'es'],
-  defaultLanguage: 'en'
+document.addEventListener('DOMContentLoaded', function() {
+  var languageFlag = document.getElementById('current-lang');
+  var enButton = document.getElementById('en-btn');
+  var esButton = document.getElementById('es-btn');
+
+  // Retrieve the selected language from localStorage
+  var storedLanguage = localStorage.getItem('selectedLanguage') || 'en';
+  var imagePath = localStorage.getItem('selectedLanguageFlag') || 'img/united-kingdom.png';
+
+  var i18n = domI18n({
+    selector: '[data-translatable]',
+    separator: ' // ',
+    languages: ['en', 'es'],
+    defaultLanguage: 'en',
+    currentLanguage: storedLanguage,
+  });
+
+  // Set the initial language flag
+  languageFlag.src = imagePath;
+
+  enButton.onclick = function () {
+    i18n.changeLanguage('en');
+    localStorage.setItem('selectedLanguage', 'en');
+    localStorage.setItem('selectedLanguageFlag', 'img/united-kingdom.png');
+    languageFlag.src = 'img/united-kingdom.png';
+  };
+
+  esButton.onclick = function () {
+    i18n.changeLanguage('es');
+    localStorage.setItem('selectedLanguage', 'es');
+    localStorage.setItem('selectedLanguageFlag', 'img/spain.png');
+    languageFlag.src = 'img/spain.png';
+  };
+
+  window.addEventListener('beforeunload', function (event) {
+    // Save the language flag before leaving the page
+    localStorage.setItem('selectedLanguageFlag', languageFlag.src);
+  });
 });
-
-enButton.onclick = function () {
-  i18n.changeLanguage('en');
-  languageFlag.src = 'img/united-kingdom.png'
-};
-
-esButton.onclick = function () {
-  i18n.changeLanguage('es');
-  languageFlag.src = 'img/spain.png'
-};
